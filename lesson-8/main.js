2ddff5d1fef74fe31ff9e7277e1616a093434510
Diego <d.senkov29@gmail.com>
2020-12-06 18:28:35 +0200

    lesson-8

---
+++ lesson-8/main.js
@@ -0,0 +1,12 @@
+let number = 10;
+
+if (number % 2 == 0) {
+    alert('Чётное!')
+} else {
+    alert ('Нечётное!')
+}
+
+let firstName = 'Денис'
+let lastName = 'Сеньков'
+
+console.log('Здравствуй, ' + firstName + ' ' + lastName + '!')
