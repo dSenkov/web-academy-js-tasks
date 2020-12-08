2402eb1af7f13fb8489f08fea7d01237e3f2fd72
Diego <d.senkov29@gmail.com>
2020-12-08 13:21:08 +0200

    accumulator bugs

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -17,7 +17,35 @@ function Calculator() {
   };
 }
 
-let calculator = new Calculator();
-calculator.read();
-alert("Sum = " + calculator.sum());
-alert("Mul = " + calculator.mul());
+function testCalculator() {
+  let calculator = new Calculator();
+  calculator.read();
+  alert("Sum = " + calculator.sum());
+  alert("Mul = " + calculator.mul());
+}
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function () {
+    alert("value = " + this.value + '. Start of function call.');
+    let additional = Number(prompt("How many to add ?"));
+    alert("additional = " + additional);
+    if (isNaN(additional)) {
+      alert("Please enter valid number!");
+      this.read();
+    }
+    this.value += additional;
+    alert("value = " + this.value + '. End of function call.');
+  };
+}
+
+function testAccumulator() {
+  let accumulator = new Accumulator(4);
+  accumulator.read();
+  accumulator.read();
+  alert(accumulator.value);
+}
+
+// testCalculator()
+testAccumulator();
